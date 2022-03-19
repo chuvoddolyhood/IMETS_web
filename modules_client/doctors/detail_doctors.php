b11e4e8a9773ea2e08016489422e431f3299fd00
Tran Nhan Nghia <63031237+chuvoddolyhood@users.noreply.github.com>
2022-03-19 21:11:32 +0700

    Update detail_doctors.php
    Design detail doctor page for client side

--- modules_client/doctors/detail_doctors.php
+++ modules_client/doctors/detail_doctors.php
@@ -8,6 +8,7 @@
 			WHERE staff.ID_Staff=$ID_Staff";
     $query = mysqli_query($conn, $sql);
 	$rows = mysqli_fetch_array($query);
+	$dept_name = $rows["Name_Dept"];
 ?>
 
 <section class="doctorDetail">
@@ -73,6 +74,17 @@
 			</div>
 		</div>
 	</div>
+
+	<div class="home-text">
+		<h1>Bạn muốn đăng ký đặt lịch với Bác sĩ?</h1>
+		<p class="animate-text">
+			<span>Kiểm tra lịch bên dưới.</span>
+			<span>Bấm vào nút đăng ký theo thời gian bạn muốn.</span>
+			<span>Chờ xác nhận đặt lịch.</span>
+			<span>Kiểm tra thông tin và thời gian</span>
+			<span>Đến bệnh viện vào đúng thời gian và địa điểm</span>
+		</p>
+	</div>
     <div style="overflow-x: auto;">
         <table>
             <thead>
@@ -107,6 +119,60 @@
         </table>
     </div>
 
+	<iframe allow="microphone;" width="350" height="430" src="https://console.dialogflow.com/api-client/demo/embedded/b2bf0f49-fe97-4245-92d8-70f39718bc37"></iframe>
+	
+	<div class="doctorList">
+		<?php
+			include './config.php';
+			$sql_get_anotherDoctor = "SELECT * FROM staff JOIN dept ON dept.ID_Dept=staff.ID_Dept
+				JOIN img_staff ON staff.ID_Staff=img_staff.ID_Staff
+				WHERE staff.Position != 'Admin' AND dept.Name_Dept='$dept_name' AND staff.ID_Staff!=$ID_Staff
+				GROUP BY staff.ID_Staff
+				ORDER BY staff.ID_Staff ASC";
+			$query_get_anotherDoctor = mysqli_query($conn, $sql_get_anotherDoctor);
+		?>
+
+		<h1> Các bác sĩ thuộc <?php echo $dept_name; ?> khác </h1>
+		<div class="container-card">
+
+			<?php
+			while($rows_get_anotherDoctor = mysqli_fetch_array($query_get_anotherDoctor)){
+			?>
+
+				<div class="card">
+					<div class="card-image">
+						<img src="./img/staff/<?php echo $rows_get_anotherDoctor["imgName"] ?>">
+					</div>
+					<h2><?php echo $rows_get_anotherDoctor["Name"] ?></h2>
+					<h3><?php echo $rows_get_anotherDoctor["Position"] ?></h3>
+					<h3><?php echo $rows_get_anotherDoctor["Name_Dept"] ?></h3>
+					<!-- <h3><?php echo $rows_get_anotherDoctor["VoteRate"] ?></h3> -->
+					
+					<div class="rating-slider">
+						<?php
+							$numberOfStar = round($rows_get_anotherDoctor["VoteRate"]);
+							// echo 'full:'.$numberOfStar;
+							$fullStart=0;
+							while($fullStart < $numberOfStar){
+							$fullStart++;
+						?>
+							<i class="fas fa-star"></i>
+						<?php }
+							$numberOfStar_empty = 5-round($rows_get_anotherDoctor["VoteRate"]);
+							// echo 'empty:'.$numberOfStar_empty;
+							$i=0;
+							while($i < $numberOfStar_empty){
+							$i++;
+						?>
+							<i class="far fa-star"></i>
+						<?php } ?>
+					</div>
+					<a href="./index.php?page_layout=detail_doctors&id= <?php echo $rows_get_anotherDoctor['ID_Staff'] ?>">Xem thông tin</a>
+					<a href="">Đặt lịch</a>
+				</div>
+			<?php } ?>
+		</div>
+	</div>
 
     <script type="text/javascript">
 		let thumbnails = document.getElementsByClassName('thumbnail_detail')
@@ -143,6 +209,34 @@
 		function confirm_Booking(){
         	return confirm("Bạn có chắc muốn chọn ngày này để đến khám ?");
     	}
+
+		const txts=document.querySelector(".animate-text").children,
+		txtsLen=txts.length;
+		let index=0;
+		const textInTimer=3000,
+		textOutTimer=2800;
+
+		function animateText() {
+			for(let i=0; i<txtsLen; i++){
+				txts[i].classList.remove("text-in","text-out");
+			}
+
+			txts[index].classList.add("text-in");
+
+			setTimeout(function(){
+				txts[index].classList.add("text-out");
+			},textOutTimer)
+
+			setTimeout(function(){
+				if(index == txtsLen-1){
+					index=0;
+				}else{
+					index++;
+				}
+				animateText();
+			},textInTimer);
+		}
+		window.onload=animateText;
 
 	</script>
 
