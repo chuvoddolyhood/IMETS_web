3cdf723f62da31b15c3e28afebbd5d5db80526e3
Nghia Tran <trannhannghia.24012000@gmail.com>
2022-03-21 20:44:28 +0700

    bug in appontment function

--- modules_client/doctors/appointment.php
+++ modules_client/doctors/appointment.php
@@ -4,7 +4,7 @@
 
     $Session_Patient = $_GET['id_Patient'];
     $ID_Staff = $_GET['id_Staff'];
-    $Date_Booking = $_GET['date'];
+    $Date_booking_appointment = $_GET['date'];
     $session = $_GET['session'];
     $room = $_GET['room'];
     $dept = $_GET['dept'];
@@ -18,81 +18,75 @@
     $ID_Patient = $rows["ID_Patient"];
 
     // =======Xu ly Date_Booking============
-    $year_booking=substr($Date_Booking, 6,4);
-    $month_booking=substr($Date_Booking, 3,2);
-    $day_booking=substr($Date_Booking, 0,2);
+    $year_booking=substr($Date_booking_appointment, 6,4);
+    $month_booking=substr($Date_booking_appointment, 3,2);
+    $day_booking=substr($Date_booking_appointment, 0,2);
     
     $time_booking=0;
     $dmy_today="$year_booking-$month_booking-$day_booking";
     
 
-    if($today<"$dmy_today 17:00:00"){
-        echo 'yes';
-    }else{echo 'no';}
+    
+    if($session=='Sáng'){
+        $sql_get_numberOfPatient = "SELECT COUNT(*) AS numberOfPatient FROM appointment WHERE Date_Checkup>='$dmy_today 07:30:00' AND Date_Checkup<='$dmy_today 11:30:00'";
+        $query_get_numberOfPatient = mysqli_query($conn, $sql_get_numberOfPatient);
+        $rows_get_numberOfPatient = mysqli_fetch_array($query_get_numberOfPatient);
+        $numberOfPatient = $rows_get_numberOfPatient["numberOfPatient"];
 
-    // if($session=='Sáng'){
-    //     $sql_get_numberOfPatient = "SELECT COUNT(*) AS numberOfPatient FROM appointment WHERE Date_Checkup>='$dmy_today 07:30:00' AND Date_Checkup<='$dmy_today 11:30:00'";
-    //     $query_get_numberOfPatient = mysqli_query($conn, $sql_get_numberOfPatient);
-    //     $rows_get_numberOfPatient = mysqli_fetch_array($query_get_numberOfPatient);
-    //     $numberOfPatient = $rows_get_numberOfPatient["numberOfPatient"];
+        // echo  'Soluongbenhnhan= '.$numberOfPatient;
+        if($numberOfPatient<9 && strtotime($today)<strtotime("$dmy_today 11:30:00")){
+            $time_stamp = strtotime("$dmy_today 07:30:00");
+            $new = $time_stamp + $numberOfPatient*30*60;
+            // echo 'Thoi gian uoc tinh'. date('Y-m-d H:i:s',$new);
+            $Date_Booking=date('Y-m-d H:i:s',$new);
+        } else {
+            echo 'Het luot';
+        }
 
-    //     echo  'Soluongbenhnhan= '.$numberOfPatient;
-    //     if($numberOfPatient<9 && $today<"$dmy_today 11:30:00"){
-    //         $time_sang="$dmy_today 07:30:00";
-    //         $time_stamp = strtotime($time_sang);
-    //         $new = $time_stamp + $numberOfPatient*30*60;
-    //         echo 'Thoi gian uoc tinh'. date('d-m-Y H:i:s',$new);
-    //     } else {
-    //         echo 'Het luot';
-    //     }
+    } elseif($session=='Chiều') {
+        $sql_get_numberOfPatient = "SELECT COUNT(*) AS numberOfPatient FROM appointment WHERE Date_Checkup>='$dmy_today 13:00:00' AND Date_Checkup<='$dmy_today 17:00:00'";
+        $query_get_numberOfPatient = mysqli_query($conn, $sql_get_numberOfPatient);
+        $rows_get_numberOfPatient = mysqli_fetch_array($query_get_numberOfPatient);
+        $numberOfPatient = $rows_get_numberOfPatient["numberOfPatient"];
 
-    // } elseif($session=='Chiều') {
-    //     $sql_get_numberOfPatient = "SELECT COUNT(*) AS numberOfPatient FROM appointment WHERE Date_Checkup>='$dmy_today 13:00:00' AND Date_Checkup<='$dmy_today 17:00:00'";
-    //     $query_get_numberOfPatient = mysqli_query($conn, $sql_get_numberOfPatient);
-    //     $rows_get_numberOfPatient = mysqli_fetch_array($query_get_numberOfPatient);
-    //     $numberOfPatient = $rows_get_numberOfPatient["numberOfPatient"];
-
-    //     echo  'Soluongbenhnhan= '.$numberOfPatient;
-    //     if($numberOfPatient<9 && $today<"$dmy_today 17:00:00"){
-    //         $time_sang="$dmy_today 13:00:00";
-    //         $time_stamp = strtotime($time_sang);
-    //         $new = $time_stamp + $numberOfPatient*30*60;
-    //         echo 'Thoi gian uoc tinh'. date('d-m-Y H:i:s',$new);
-    //     } else {
-    //         echo 'Het luot';
-    //     }
+        // echo  'Soluongbenhnhan= '.$numberOfPatient;
+        if($numberOfPatient<9 && strtotime($today)<strtotime("$dmy_today 17:00:00")){
+            $time_stamp = strtotime("$dmy_today 13:00:00");
+            $new = $time_stamp + $numberOfPatient*30*60;
+            // echo 'Thoi gian uoc tinh'. date('Y-m-d H:i:s',$new);
+            $Date_Booking=date('Y-m-d H:i:s',$new);
+        } else {
+            echo 'Het luot';
+        }
 
 
 
-    // }elseif($session=='Ngoài giờ') {
-    //     $sql_get_numberOfPatient = "SELECT COUNT(*) AS numberOfPatient FROM appointment WHERE Date_Checkup>='$dmy_today 18:30:00' AND Date_Checkup<='$dmy_today 21:30:00'";
-    //     $query_get_numberOfPatient = mysqli_query($conn, $sql_get_numberOfPatient);
-    //     $rows_get_numberOfPatient = mysqli_fetch_array($query_get_numberOfPatient);
-    //     $numberOfPatient = $rows_get_numberOfPatient["numberOfPatient"];
-    //     echo  'Soluongbenhnhan= '.$numberOfPatient;
+    }elseif($session=='Ngoài giờ') {
+        $sql_get_numberOfPatient = "SELECT COUNT(*) AS numberOfPatient FROM appointment WHERE Date_Checkup>='$dmy_today 18:30:00' AND Date_Checkup<='$dmy_today 21:30:00'";
+        $query_get_numberOfPatient = mysqli_query($conn, $sql_get_numberOfPatient);
+        $rows_get_numberOfPatient = mysqli_fetch_array($query_get_numberOfPatient);
+        $numberOfPatient = $rows_get_numberOfPatient["numberOfPatient"];
+        echo  'Soluongbenhnhan= '.$numberOfPatient;
         
-    //     if($numberOfPatient<9 && $today<"$dmy_today 21:30:00"){
-    //         $time_sang="$dmy_today 18:30:00";
-    //         $time_stamp = strtotime($time_sang);
-    //         $new = $time_stamp + $numberOfPatient*30*60;
-    //         echo 'Thoi gian uoc tinh'. date('d-m-Y H:i:s',$new);
-    //     } else {
-    //         echo 'Het luot';
-    //     }
-    // }
+        if($numberOfPatient<6 && strtotime($today)<strtotime("$dmy_today 21:30:00")){
+            $time_stamp = strtotime("$dmy_today 18:30:00");
+            $new = $time_stamp + $numberOfPatient*30*60;
+            echo 'Thoi gian uoc tinh'. date('Y-m-d H:i:s',$new);
+            $Date_Booking=date('Y-m-d H:i:s',$new);
+        } else {
+            echo 'Het luot';
+        }
+    }
 
     // echo $ID_Staff;
     // echo $ID_Patient;
     // echo $today;
     // echo $Date_Booking;
 
-    
-    // echo $Date_Booking;
-    // 2022-12-31 00:00:00
 
-    // $sql_add_appointment = "INSERT INTO appointment(`ID_Staff`, `ID_Patient`, `Date_Booking`, `Date_Checkup`) 
-    //     VALUES ($ID_Staff,$ID_Patient,'$today','2022-03-21 23:59:59')";
-	// $query_add_appointment = mysqli_query($conn, $sql_add_appointment);
+    $sql_add_appointment = "INSERT INTO appointment(`ID_Staff`, `ID_Patient`, `Date_Booking`, `Date_Checkup`) 
+        VALUES ($ID_Staff,$ID_Patient,'$today','$Date_Booking')";
+	$query_add_appointment = mysqli_query($conn, $sql_add_appointment);
 
-    // header('location: ./../../index.php');
+    header('location: ./../../index.php');
 ?>
--- modules_client/doctors/detail_doctors.php
+++ modules_client/doctors/detail_doctors.php
@@ -183,6 +183,10 @@
 	</div>
 
 
+
+
+
+	
 	<div id="chatbot">
 		<div id="chatbot-icon">
 			<i class="fas fa-star chatbot-icon"></i>
@@ -195,82 +199,6 @@
 	</div>
 
 	<!-- ======================Điều chỉnh CSS list_doctor.php====================== -->
-    <script type="text/javascript">
-		const chatbot = document.getElementById('chatbot');
-		const chatbot_icon = document.getElementById('chatbot-icon');
-		const chatbot_box = document.getElementById('chatbot-box');
-
-		if(chatbot_icon){
-			chatbot_icon.addEventListener('click', ()=>{
-				chatbot_box.classList.add('chatbot_active');
-				chatbot_icon.classList.add('chatbot_unactive');
-			})
-		}
-
-
-		let thumbnails = document.getElementsByClassName('thumbnail_detail')
-
-		let activeImages = document.getElementsByClassName('active')
-
-		for (var i=0; i < thumbnails.length; i++){
-
-			thumbnails[i].addEventListener('mouseover', function(){
-				console.log(activeImages)
-				
-				if (activeImages.length > 0){
-					activeImages[0].classList.remove('active')
-				}
-				
-
-				this.classList.add('active')
-				document.getElementById('featured_detail').src = this.src
-			})
-		}
-
-
-		let buttonRight = document.getElementById('slideRight_detail');
-		let buttonLeft = document.getElementById('slideLeft_detail');
-
-		buttonLeft.addEventListener('click', function(){
-			document.getElementById('slider_detail').scrollLeft -= 180
-		})
-
-		buttonRight.addEventListener('click', function(){
-			document.getElementById('slider_detail').scrollLeft += 180
-		})
-
-		function confirm_Booking(){
-        	return confirm("Bạn có chắc muốn chọn ngày này để đến khám ?");
-    	}
-
-		const txts=document.querySelector(".animate-text").children,
-		txtsLen=txts.length;
-		let index=0;
-		const textInTimer=3000,
-		textOutTimer=2800;
-
-		function animateText() {
-			for(let i=0; i<txtsLen; i++){
-				txts[i].classList.remove("text-in","text-out");
-			}
-
-			txts[index].classList.add("text-in");
-
-			setTimeout(function(){
-				txts[index].classList.add("text-out");
-			},textOutTimer)
-
-			setTimeout(function(){
-				if(index == txtsLen-1){
-					index=0;
-				}else{
-					index++;
-				}
-				animateText();
-			},textInTimer);
-		}
-		window.onload=animateText;
-
-	</script>
+    <script src="./modules_client/doctors/detail_doctor.js"></script>
 
 </section>
